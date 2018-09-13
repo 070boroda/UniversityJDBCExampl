3cdd8168afbcaa7b348f8df8491fc0cf2b7c472e
ALIAKSANDR <zely1984@gmail.com>
2018-09-13 19:06:28 +0300

    add toString teacher

--- src/com/foxminded/universety/Teacher.java
+++ src/com/foxminded/universety/Teacher.java
@@ -1,19 +1,28 @@
 package com.foxminded.universety;
 
 public class Teacher {
-	private String firstName;
-	private String secondName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getSecondName() {
-		return secondName;
-	}
-	public void setSecondName(String secondName) {
-		this.secondName = secondName;
-	}
+    private String firstName;
+    private String secondName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + secondName;
+    }
+
 }
