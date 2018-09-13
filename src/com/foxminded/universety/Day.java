057f0335a40ead749916838de601048805b5f655
ALIAKSANDR <zely1984@gmail.com>
2018-09-13 19:26:41 +0300

    add day

---
+++ src/com/foxminded/universety/Day.java
@@ -0,0 +1,5 @@
+package com.foxminded.universety;
+
+public class Day {
+
+}
---
+++ src/com/foxminded/universety/Schedule.java
@@ -0,0 +1,19 @@
+package com.foxminded.universety;
+
+import java.awt.List;
+
+public class Schedule {
+
+    private Subject subject;
+    private String startTime;
+    private List dayOfWeek;
+    private Group groupNumber;
+    private Teacher teacher;
+    private LectureHall lectureHall;
+
+    public void createScheduleDay(Subject subject, String startTime, String dayOfWeek, Group groupNumber,
+            Teacher teacher, LectureHall lectureHall) {
+
+    }
+
+}
