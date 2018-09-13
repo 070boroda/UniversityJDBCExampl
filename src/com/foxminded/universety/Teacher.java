52e94568daa76cadb9f609c5d7e63f050aeae0e1
1 <1@boroda>
2018-09-13 13:38:12 +0300

    add teacher

---
+++ src/com/foxminded/universety/Teacher.java
@@ -0,0 +1,19 @@
+package com.foxminded.universety;
+
+public class Teacher {
+	private String firstName;
+	private String secondName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+}
