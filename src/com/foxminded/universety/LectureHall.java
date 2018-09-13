48e443eeb4c085a2709d83bc2809c3e68bcc0caa
1 <1@boroda>
2018-09-13 13:39:46 +0300

    add lecturehall

---
+++ src/com/foxminded/universety/LectureHall.java
@@ -0,0 +1,13 @@
+package com.foxminded.universety;
+
+public class LectureHall {
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
