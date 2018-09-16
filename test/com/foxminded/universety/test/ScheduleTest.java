0f9d54f366d7d95f8a0fa5f7cb447f46f8d92c87
ALIAKSANDR <zely1984@gmail.com>
2018-09-16 19:21:12 +0300

    add test file

---
+++ test/com/foxminded/universety/test/ScheduleTest.java
@@ -0,0 +1,24 @@
+package com.foxminded.universety.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScheduleTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testSetDayName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddSchedule() {
+        fail("Not yet implemented");
+    }
+
+}
