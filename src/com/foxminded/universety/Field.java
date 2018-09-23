628fa5871969e701c95983cd9abaa7f5c427dd8c
ALIAKSANDR <zely1984@gmail.com>
2018-09-23 17:44:33 +0300

    refactor

--- src/com/foxminded/universety/Field.java
+++ src/com/foxminded/universety/Field.java
@@ -1,7 +1,5 @@
 package com.foxminded.universety;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
--- src/com/foxminded/universety/Teacher.java
+++ src/com/foxminded/universety/Teacher.java
@@ -1,7 +1,6 @@
 package com.foxminded.universety;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
