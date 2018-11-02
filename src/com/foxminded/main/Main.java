7c1adba2399d766e783840df5763b7bf9b6c3870
ALIAKSANDR <zely1984@gmail.com>
2018-11-02 22:08:41 +0300

    ref main

--- src/com/foxminded/main/Main.java
+++ src/com/foxminded/main/Main.java
@@ -7,7 +7,6 @@ import com.foxminded.dao.StudentDao;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
         StudentDao studD = new StudentDao();
         studD.getById(1);
 
