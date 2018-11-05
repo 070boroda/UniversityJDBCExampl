c32e9c9efe3245adcc93547a6b1280fffed4bfc2
1 <1@boroda>
2018-11-05 14:23:45 +0300

    ref main

--- src/com/foxminded/main/Main.java
+++ src/com/foxminded/main/Main.java
@@ -1,7 +1,6 @@
 package com.foxminded.main;
 
 import java.sql.SQLException;
-import com.foxminded.entity.University;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -10,8 +9,6 @@ public class Main {
     public static void main(String[] args) throws SQLException {
     	
     	log.info("Start main");
-        University un = new University();
-        un.createUniversity();
 
     }
 }
